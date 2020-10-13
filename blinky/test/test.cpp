357cccc460291ef1aeee121de1736190b754cdcb
Doug Schaefer <dschaefer2@apple.com>
2020-10-12 23:01:38 -0400

    Fixed up test to account for reset.

--- blinky/test/test.cpp
+++ blinky/test/test.cpp
@@ -4,6 +4,7 @@ TEST_F(BlinkyTest, Test0) {
     ASSERT_EQ(blinky.led, 0);
     int last = 0;
     int cycles = 0;
+    blinky.nreset = 1;
     tick();
     for (uint32_t count = 1; count <= 1 << 28; count++) {
         tick();
