a0d0f369c8588ac189890d6f01fd2bdbedf8fb29
Doug Schaefer <cdtdoug@gmail.com>
2021-11-05 21:27:42 -0400

    Added PLL.

--- top.sv
+++ top.sv
@@ -4,9 +4,20 @@ module top(
     output [7:0] led
 );
 
+    wire clk_100mhz;
+    wire nreset, locked;
+
+    clocks clocks(
+        .clkin(clk_25mhz),
+        .clkout0(clk_100mhz),
+        .locked(locked)
+    );
+
+    assign nreset = btn[0] & locked;
+
     blinky blink(
-        .clk(clk_25mhz),
-        .nreset(btn[0]),
+        .clk(clk_100mhz),
+        .nreset(nreset),
         .led(led)
     );
 
